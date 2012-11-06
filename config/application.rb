6c3f3af38e202e1d85460afec50ce3fb64ef6fd3
wangxz <wangxz@csdn.net>
2012-11-06 17:23:25 +0800

    asset pipline

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(:assets => %w(development test production)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -49,6 +49,7 @@ module CodeCampo
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
